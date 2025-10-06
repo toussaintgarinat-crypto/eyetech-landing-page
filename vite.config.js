aff180ee888a569f517206b50651ad5271c9ce0f
hikarechi <toussaintgarinat@gmail.com>
2025-10-06 20:37:33 +0200

    "FixPagesPath"

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/eyetech-landing-page/",
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
